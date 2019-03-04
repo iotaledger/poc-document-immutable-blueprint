85015da324336da4eb82acab62531ce69337a101
Marwen trabelsi <marwen.projob@gmail.com>
2019-03-04 14:49:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+  ## Document Immutability check
+
+  # Before starting
+
+  due to some restriction about the Cross-origin of *thetange.org* - it blocks all connections coming from unkown hosts like yours - we are required to install this plugin to override the cross-origin automatically: https://chrome.google.com/webstore/detail/moesif-orign-cors-changer/digfbfaphojjndkpccljibejjbppifbc
+
+  make sure to enable it.
+
+  Please also enable it before usage.
---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { verify } from 'iota-sig2'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      file: null,
+      transactionHash: ''
+    }
+    this.handleFileSet = this.handleFileSet.bind(this)
+    this.verify = this.verify.bind(this)
+    this.reset = this.reset.bind(this)
+    this.handleInputTextChange = this.handleInputTextChange.bind(this)
+  }
+  handleInputTextChange(e) {
+    this.setState({
+      transactionHash: e.target.value
+    })
+  }
+  verify(e) {
+    const reader = new FileReader();
+    reader.addEventListener("loadend", () => {
+       verify(
+              this.state.transactionHash,
+              true,
+              reader.result,
+              (verified) => console.log(verified)
+             )
+    });
+    reader.readAsArrayBuffer(this.state.file);
+  }
+  reset(e) {
+
+  }
+  handleFileSet(e) {
+    this.setState({
+      file: e.target.files[0]
+    })
+  }
+  render() {
+    return (
+      <div className="layouts--search">
+         <div className="middle-column">
+          <div className="button-container button-container__center">
+            <input className="button button--secondary"
+                   type="file"
+                   id="input"
+                   onChange={this.handleFileSet}
+            />
+          </div>
+          <div className="button-container button-container__center">
+            <input className="button button--secondary"
+                   type="text"
+                   id="input"
+                   value={this.state.transactionHash}
+                   onChange={this.handleInputTextChange}
+            />
+          </div>
+           <div className="button-container button-container__center">
+            <button className="button" onClick={this.verify}>Verify</button>
+            <button className="button" onClick={this.reset}>Reset</button>
+           </div>
+         </div>
+       </div>
+    );
+  }
+}
+
+export default App;
