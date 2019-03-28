e4e715ead887a21674ae254cf014b91e6fb94eba
mtrabelsi <marwen.projob@gmail.com>
2019-03-28 15:51:42 +0100

    restructured the UI and added auto hash calculation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { verify } from 'signature-validation-tool'
+import { verify, hash } from 'signature-validation-tool'
 import nodes from './nodes'
 import DropDown from './Dropdown'
 //IFWT9BLRWACAJHCMTBKDPYRDAWUQSYVIPTLQSRKJJ9XCIPVXTZSNSABBRWEQDVOGIVHFLXZGI9XSA9999
@@ -13,7 +13,11 @@ class App extends Component {
       file: null,
       transactionHash: '',
       provider: '',
-      address: ''
+      address: '',
+      docMutated: null,
+      hashValue: '',
+      genTxHash: '',
+      genChannelAddress: ''
     }
     this.handleFileSet = this.handleFileSet.bind(this)
     this.verify = this.verify.bind(this)
@@ -51,7 +55,7 @@ class App extends Component {
               bundle,
               true,
               file,
-              (verified) => console.log(verified)
+              (verified) => this.setState({docMutated: verified})
              )
     });
     reader.readAsArrayBuffer(this.state.file);
@@ -60,28 +64,73 @@ class App extends Component {
 
   }
   handleFileSet(e) {
-    this.setState({
-      file: e.target.files[0]
-    })
+    const file = e.target.files[0]
+    this.setState({file})
+    const reader = new FileReader();
+    reader.addEventListener("loadend", () => {
+       const file = reader.result
+       const hashValue = hash(file, true)
+       this.setState({hashValue})
+    });
+    reader.readAsArrayBuffer(file);
   }
   render() {
+    const docMutated = this.state.docMutated
+    const title = 'Please fill the form'
+    const text = `Please fill the form above, make sure you pick up the right provider.
+    If you don't have TX Hash or Chnnel Address yet, then you have to publish your document and get these infos.`
+    const cssClass = 'message-box__info'
+    if(docMutated === 0) {
+      cssClass = 'message-box__danger'
+      const title = 'Document not valid anymore'
+      const text = `The calculated Hash didn't match with the saved one`
+      const cssClass = 'message-box__info'
+    } else if(docMutated === 1) {
+      cssClass = 'message-box__success'
+      const title = 'Document Valid!'
+      const text = `The document hash haven't been mutated since its first signature with the Tangle.`
+      const cssClass = 'message-box__info'
+    }
     return (
       <div className="layouts--search">
          <div className="middle-column">
-          <div className="button-container button-container__center">
-            <input className="button button--secondary"
-                   type="file"
-                   id="input"
-                   onChange={this.handleFileSet}
-            />
-          </div>
-          <div className="button-container button-container__center">
-            <DropDown
-              nodes={nodes}
-              onProviderSelected={this.onProviderSelected}
-              styles={styles}
-            />
-          </div>
+           <h1 className="text text--level1 text--primary">General params</h1>
+           <div className="button-container button-container__center">
+             <input className="button button--secondary"
+                    type="file"
+                    id="input"
+                    onChange={this.handleFileSet}
+             />
+           </div>
+           <div className="button-container button-container__center">
+             <DropDown
+               nodes={nodes}
+               onProviderSelected={this.onProviderSelected}
+               styles={styles}
+             />
+           </div>
+
+           <div className="horizontal-rule"></div>
+           <h1 className="text text--level1 text--primary">Document Signature</h1>
+
+
+           <div className="button-container button-container__center">
+            <button className="button" onClick={this.signDocument}>Sign the document</button>
+            <button className="button" onClick={this.reset}>Clear</button>
+           </div>
+
+           <div className="button-container button-container__center">
+              <div style={{display: 'block'}}>
+                <div>{this.state.hashValue}</div>
+                <div>{this.state.genTxHash}</div>
+                <div>{this.state.genChannelAddress}</div>
+              </div>
+           </div>
+
+           <div className="horizontal-rule"></div>
+
+           <h1 className="text text--level1 text--primary">Document Validation</h1>
+
           <div className="button-container button-container__center">
             <input className="button button--secondary"
                    type="text"
@@ -108,6 +157,17 @@ class App extends Component {
             <button className="button" onClick={this.verify}>Verify</button>
             <button className="button" onClick={this.reset}>Reset</button>
            </div>
+           <br/>
+           <div className="button-container button-container__center">
+              <div className={`message-box ${cssClass}`} >
+                <div className="message-box--icon"></div>
+                <div className="message-box--text">
+                   <div className="message-box--title">{title}</div>
+                   <div className="message-box--content">{text}</div>
+                </div>
+              </div>
+          </div>
+
          </div>
        </div>
     );
