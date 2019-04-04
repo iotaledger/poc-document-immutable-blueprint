1b80e9d162d77ba1ae6e0acf37ccbe5c9144a7cb
mtrabelsi <marwen.projob@gmail.com>
2019-04-04 19:01:51 +0200

    Fixed Fonts

---
+++ public/assets/icons/icons.js
@@ -0,0 +1,19 @@
+module.exports = {
+   "arrow-right": "→",
+   "burger": "🍔",
+   "chevron-up": "↑",
+   "chevron-down": "↓",
+   "chevron-right": ">",
+   "chevron-left": "<",
+   "cross": "X",
+   "copy": "📄",
+   "feedback": "💬",
+   "search": "🔍",
+   "thumb-up": "👍",
+   "thumb-down": "👎",
+   "up-down": "🔽",
+   "success": "✅",
+   "danger": "⛔",
+   "info": "i",
+   "warning": "!"
+}
