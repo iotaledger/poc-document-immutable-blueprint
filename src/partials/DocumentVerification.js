0c35b5eabf4c3cbc44472fdc53b8ad20b9f1e898
Marwen trabelsi <marwen.projob@gmail.com>
2019-04-24 19:42:38 +0200

    enhanced Validation page UX and usability

--- src/partials/DocumentVerification.js
+++ src/partials/DocumentVerification.js
@@ -24,54 +24,60 @@ class App extends Component {
   render() {
     let docMutated = this.props.docMutated
     let title = `Let the Tangle validate it`
+    let text = ''
     let validText = false
     if(docMutated === false) {
       title = 'Document has been changed.'
+      text = 'Looks like the Document have been mutated, the calculated hash has changed ever since we signed it with the Tangle.'
     } else if(docMutated === true) {
       title = 'Document Valid!'
+      text = 'Tangle Signature valid.'
       validText = true
     }
     return(<div>
       <div>
         <Title value={title} valid={validText} />
-        <p>Please fill the generated TX Hash (should be in your clipboard) and the Fetching Address, if you don't have them yet, please go to Step 2</p>
+        {!docMutated && <p>{text}</p>}
       </div>
-      <div>
-        <input className="button button--secondary"
-               type="text"
-               id="input"
-               name="txhash"
-               style={styles}
-               placeholder="TX Hash"
-               value={this.props.transactionHash}
-               onChange={this.props.handleInputTextChange}
-        />
-      </div>
-      <div>
-        <input className="button button--secondary"
-               type="text"
-               id="input"
-               name="address"
-               style={styles}
-               placeholder="Fetch Address"
-               value={this.props.address}
-               onChange={this.props.handleInputTextChange}
-        />
-      </div>
-       <div>
-        <button className="button" onClick={this.props.verify}>Verify</button>
-        <button className="button" onClick={this.props.reset}>Reset</button>
-       </div>
-       <br/>
-       {/*<div className="button-container button-container__center">
-          <div className={`message-box ${cssClass}`} >
-            <div className="message-box--icon"></div>
-            <div className="message-box--text">
-               <div className="message-box--title">{title}</div>
-               <div className="message-box--content">{text}</div>
-            </div>
+
+      {docMutated && <div style={{ marginTop: '40px' }} className="message-box message-box__success">
+        <div className="message-box--icon"></div>
+        <div className="message-box--text">
+          <div className="message-box--title">{title}</div>
+          <div className="message-box--content">
+            {text}
           </div>
-      </div>*/}
+        </div>
+      </div>}
+      {!docMutated &&<section>
+         <p>Please fill the generated TX Hash (should be in your clipboard) and the Fetching Address, if you don't have them yet, please go to Step 2</p>
+         <div>
+            <input className="button button--secondary"
+                   type="text"
+                   id="input"
+                   name="txhash"
+                   style={styles}
+                   placeholder="TX Hash"
+                   value={this.props.transactionHash}
+                   onChange={this.props.handleInputTextChange}
+            />
+         </div>
+         <div>
+            <input className="button button--secondary"
+                   type="text"
+                   id="input"
+                   name="address"
+                   style={styles}
+                   placeholder="Fetch Address"
+                   value={this.props.address}
+                   onChange={this.props.handleInputTextChange}
+            />
+         </div>
+         <div>
+            <button className="button" onClick={this.props.verify}>Verify</button>
+            <button className="button" onClick={this.props.reset}>Reset</button>
+         </div>
+       </section>}
       </div>)
   }
 }
