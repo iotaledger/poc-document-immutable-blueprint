63d32ab59a4a25920939aca5625c16ef3caa706e
Marwen trabelsi <marwen.projob@gmail.com>
2019-04-24 21:34:26 +0200

    Cleanup more dead/commented code

--- src/App.js
+++ src/App.js
@@ -126,11 +126,6 @@ class App extends Component {
     reader.readAsArrayBuffer(file);
   }
 
-
-
-
-
-
   render() {
     return (
         <Router>
@@ -181,124 +176,6 @@ class App extends Component {
         </Router>
       );
   }
-  /*render() {
-    let docMutated = this.state.docMutated
-    let title = 'Please fill the form'
-    let text = `Please fill the form above, make sure you pick up the right provider.
-    If you don't have TX Hash or Chnnel Address yet, then you have to publish your document and get these infos.`
-    let cssClass = 'message-box__info'
-    if(docMutated === false) {
-      cssClass = 'message-box__danger'
-      title = 'Document not valid anymore'
-      text = `The calculated Hash didn't match with the saved one`
-    } else if(docMutated === true) {
-      cssClass = 'message-box__success'
-      title = 'Document Valid!'
-      text = `The document hash haven't been mutated since its first signature with the Tangle.`
-    }
-    return (
-      <div className="layouts--search">
-         <div className="middle-column">
-           <h1 className="text text--level1 text--primary">1 - General params</h1>
-           <div className="button-container button-container__center">
-             <input className="button button--secondary"
-                    type="file"
-                    id="input"
-                    onChange={this.handleFileSet}
-             />
-           </div>
-           <div className="button-container button-container__center">
-             <DropDown
-               nodes={nodes}
-               onProviderSelected={this.onProviderSelected}
-               styles={styles}
-             />
-           </div>
-
-           <div className="horizontal-rule"></div>
-           <h1 className="text text--level1 text--primary">2 - Document Signature (can be omitted)</h1>
-
-           <div className="button-container button-container__center">
-             <input className="button button--secondary"
-                    type="text"
-                    id="input"
-                    name="pubAddress"
-                    style={styles}
-                    placeholder="Publish Address, if omitted default"
-                    value={this.state.pubAddress}
-                    onChange={this.handleInputTextChange}
-             />
-           </div>
-           <div className="button-container button-container__center">
-             <input className="button button--secondary"
-                    type="text"
-                    id="input"
-                    name="pubSeed"
-                    style={styles}
-                    placeholder="Seed, if omitted default is taken"
-                    value={this.state.pubSeed}
-                    onChange={this.handleInputTextChange}
-             />
-           </div>
-           <div className="button-container button-container__center">
-            <button className="button" onClick={this.signDocument}>Sign the document</button>
-            <button className="button" onClick={this.reset}>Clear</button>
-           </div>
-
-           <div className="button-container button-container__center">
-              <div style={{display: 'block'}}>
-                <div><span className="text text--level2">Calculated Hash Value:</span></div>
-                <div>{this.state.hashValue}</div>
-                <div><span className="text text--level2">Generated TX Hash:</span></div>
-                <div>{this.state.genTxHash}</div>
-              </div>
-           </div>
-
-           <div className="horizontal-rule"></div>
-
-           <h1 className="text text--level1 text--primary">3 - Document Validation</h1>
-
-          <div className="button-container button-container__center">
-            <input className="button button--secondary"
-                   type="text"
-                   id="input"
-                   name="txhash"
-                   style={styles}
-                   placeholder="TX Hash"
-                   value={this.state.transactionHash}
-                   onChange={this.handleInputTextChange}
-            />
-          </div>
-          <div className="button-container button-container__center">
-            <input className="button button--secondary"
-                   type="text"
-                   id="input"
-                   name="address"
-                   style={styles}
-                   placeholder="Fetch Address"
-                   value={this.state.address}
-                   onChange={this.handleInputTextChange}
-            />
-          </div>
-           <div className="button-container button-container__center">
-            <button className="button" onClick={this.verify}>Verify</button>
-            <button className="button" onClick={this.reset}>Reset</button>
-           </div>
-           <br/>
-           <div className="button-container button-container__center">
-              <div className={`message-box ${cssClass}`} >
-                <div className="message-box--icon"></div>
-                <div className="message-box--text">
-                   <div className="message-box--title">{title}</div>
-                   <div className="message-box--content">{text}</div>
-                </div>
-              </div>
-          </div>
-
-         </div>
-       </div>
-    );
-  } */
 }
 
 export default App;
