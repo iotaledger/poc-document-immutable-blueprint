a733e27303c5e3b97110fbf6dc900fc40bfe1294
Marwen trabelsi <marwen.projob@gmail.com>
2019-04-26 10:25:26 +0200

    fixed class=>classname

--- src/partials/loader/index.js
+++ src/partials/loader/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './index.css'
 
 function Loader(props) {
-  return (<div class="loader" />)
+  return (<div className="loader" />)
 }
 
 export default Loader
