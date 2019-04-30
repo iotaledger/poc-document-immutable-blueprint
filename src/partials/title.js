45ed5e2f483d470f65569af083b860fa43f23c78
Marwen trabelsi <marwen.projob@gmail.com>
2019-04-30 18:40:11 +0200

    made the title and lyout more mobile friendly

--- src/partials/title.js
+++ src/partials/title.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 function Title(props) {
   return (<span
-      style={{ fontSize: '64px', color: props.valid ? '#019792' : '#603f98', ...props.style }}
-      className="text">
+      style={{ color: props.valid ? '#019792' : '#603f98', ...props.style }}
+      className="text instruction-title">
       {props.value}
     </span>)
 }
