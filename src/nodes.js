4b36f4715e32b8efc06de65431f082d6ee64f57e
Martyn Janes <martyn@obany.com>
2021-11-08 11:45:50 +0100

    chore: Update config

--- src/nodes.js
+++ src/nodes.js
@@ -1,26 +1,4 @@
 const nodes = [
-  // {
-  //   nodeId: 'https://nodes.devnet.iota.org',
-  //   isMainnet: false
-  // }, {
-  //   nodeId: 'https://nodes.thetangle.org:443',
-  //   isMainnet: true
-  // }, {
-  //   nodeId: 'https://nodes.iota.cafe',
-  //   isMainnet: true
-  // }, {
-  //   nodeId: 'https://altnodes.devnet.iota.org',
-  //   isMainnet: false
-  // }, {
-  //   nodeId: 'https://nodes.devnet.thetangle.org',
-  //   isMainnet: false
-  // }, {
-  //   nodeId: 'https://testnet140.tangle.works',
-  //   isMainnet: false
-  // }, {
-  //   nodeId: 'http://p103.testnet.iota.cafe:14700',
-  //   isMainnet: false
-  // },
   {
     nodeId: 'https://api.lb-0.h.chrysalis-devnet.iota.cafe/',
     isMainnet: false
@@ -28,19 +6,7 @@ const nodes = [
   {
     nodeId: 'https://api.lb-1.h.chrysalis-devnet.iota.cafe/',
     isMainnet: false
-  },  
-  //{
-  //  nodeId: 'https://api.lb-0.testnet.chrysalis2.com',
-  //  isMainnet: false
-  //},
-  //{
-  //  nodeId: 'https://chrysalis-nodes.iota.cafe',
-  //  isMainnet: true
-  //},
-  //{
-  //  nodeId: 'https://chrysalis-nodes.iota.org',
-  //  isMainnet: true
-  //},
+  }
 ]
 
 export default nodes
